6e70e45ceb010ed7cbc9a80c7e088aa70c83904d
Hamza <muhammadhamza1311@gmail.com>
2020-03-24 02:09:54 +0500

    show countries

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,7 @@
+import org.w3c.dom.HTMLOptionElement
+import org.w3c.dom.HTMLSelectElement
 import org.w3c.dom.HTMLSpanElement
+import utils.countries
 import kotlin.browser.document
 
 fun getSpanElement(selector: String) = document.querySelector(selector) as HTMLSpanElement
@@ -8,4 +11,14 @@ suspend fun main() {
     getSpanElement("#data-active").textContent = data.confirmed.toString()
     getSpanElement("#data-recoveries").textContent = data.recovered.toString()
     getSpanElement("#data-deaths").textContent = data.deaths.toString()
+    val pk = CoronaChanTrackerWrapper.getForLocation("PK")
+    console.log(pk)
+
+    val select = document.querySelector("#select-country") as HTMLSelectElement
+
+    countries.forEach {
+        val node = document.createElement("option") as HTMLOptionElement
+        node.innerText = "${it.key} - ${it.value}"
+        select.appendChild(node)
+    }
 }
